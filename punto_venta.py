ec9969ad09cc55936e430bf8446a0605afd76ba7
Julio César Delgado Mora <60369136+Paidcear@users.noreply.github.com>
2026-01-07 00:01:04 -0600

    Update punto_venta.py

--- punto_venta.py
+++ punto_venta.py
@@ -149,14 +149,14 @@ def render_punto_venta(ventas):
                 #total = df_carrito["subtotal"].sum()
                 st.sidebar.markdown("---")
                 st.sidebar.title("Total de la venta")
-                st.markdown(f"**Subtotal:** ${total:,.2f}")
+                st.sucess(f"**Subtotal:** ${total:,.2f}")
                 st.sidebar.markdown(
                     f"<div style='font-size: 80px; font-weight: bold; color: #2E8B57;'>${total:,.2f}</div>",
                     unsafe_allow_html=True
                 )
 
                 # REGISTRAR VENTA
-                if st.button("**Registrar venta**"):
+                if st.sidebar.button("**Registrar venta**"):
 
                     # DESCONTAR STOCK EN BD
                     for item in st.session_state.carrito:
@@ -200,4 +200,5 @@ def render_punto_venta(ventas):
                 """,
                 height=0
             )
+
 
